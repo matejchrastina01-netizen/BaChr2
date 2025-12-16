988ac6af82597ef09acc85c9b6ff29a05822f0eb
swpv <swpv@email.cz>
2025-12-16 18:06:07 +0100

    7. commit

--- UTB.BaChr.Mapy/UTB.BaChr.Mapy/wwwroot/js/site.js
+++ UTB.BaChr.Mapy/UTB.BaChr.Mapy/wwwroot/js/site.js
@@ -9,6 +9,7 @@ $(document).ready(function () {
 });
 
 function initMap() {
+    // ... (kód pro inicializaci mapy, imageBounds, maxBounds atd. zůstává stejný) ...
     var w = 2048;
     var h = 2048;
 
@@ -24,25 +25,31 @@ function initMap() {
     L.control.zoom({ position: 'topright' }).addTo(map);
     myMap = map;
 
-    // 1. Definice hranic samotného obrázku (přesně 2048x2048)
     var southWestImg = map.unproject([0, h], map.getMaxZoom() - 1);
     var northEastImg = map.unproject([w, 0], map.getMaxZoom() - 1);
     var imageBounds = new L.LatLngBounds(southWestImg, northEastImg);
 
-    // 2. Definice hranic pro pohyb (přidáme 500px "vatu" okolo, aby se neusekávaly popisky)
     var padding = 500;
     var southWestMax = map.unproject([-padding, h + padding], map.getMaxZoom() - 1);
     var northEastMax = map.unproject([w + padding, -padding], map.getMaxZoom() - 1);
     var maxBounds = new L.LatLngBounds(southWestMax, northEastMax);
 
-    // Přidání obrázku (použijeme imageBounds - aby se obrázek nenatahoval do paddingu)
     L.imageOverlay('/images/Firewatch-World-Map-Clean.jpg', imageBounds).addTo(map);
-
-    // Nastavení omezení pohybu (použijeme maxBounds - ty větší hranice)
     map.setMaxBounds(maxBounds);
-
-    // Na startu vycentrujeme na obrázek
     map.fitBounds(imageBounds);
+
+    // === NOVÉ: Definice vlastního vzhledu ikonky ===
+    // Použijeme L.divIcon, který vytvoří HTML div s naší CSS třídou
+    var firewatchIcon = L.divIcon({
+        className: '', // Necháme prázdné, třídu dáme přímo do html
+        // Vložíme div s naší CSS třídou 'firewatch-pin'
+        html: '<div class="firewatch-pin"></div>',
+        iconSize: [30, 30], // Velikost kontejneru
+        iconAnchor: [15, 30], // Bod, kterým se pin "zapíchne" do mapy (spodní špička)
+        popupAnchor: [0, -35] // Kde se otevře bublina relativně k ukotvení
+    });
+    // ==============================================
+
 
     // Vykreslení pinů
     if (typeof mapLocations !== 'undefined' && mapLocations !== null) {
@@ -50,7 +57,9 @@ function initMap() {
             if (loc.mapX != null && loc.mapY != null) {
                 var point = map.unproject([loc.mapX, loc.mapY], map.getMaxZoom() - 1);
 
-                var marker = L.marker(point).addTo(map);
+                // UPRAVENO: Přidána možnost { icon: firewatchIcon }
+                var marker = L.marker(point, { icon: firewatchIcon }).addTo(map);
+
                 var popupContent = `<b>${loc.name}</b><br>${loc.description || ''}`;
                 marker.bindPopup(popupContent);
 
@@ -60,33 +69,26 @@ function initMap() {
         });
     }
 
-    // Nástroj pro zjišťování souřadnic
+    // Nástroj pro zjišťování souřadnic (zůstává stejný)
     var popup = L.popup();
     function onMapClick(e) {
+        // ... (zbytek funkce onMapClick) ...
         var point = map.project(e.latlng, map.getMaxZoom() - 1);
         var x = Math.floor(point.x);
         var y = Math.floor(point.y);
-
         popup
             .setLatLng(e.latlng)
-            .setContent(`
-                <div style="text-align:center;">
-                    <b>Souřadnice</b><br>
-                    MapX: <code>${x}</code><br>
-                    MapY: <code>${y}</code>
-                </div>
-            `)
+            .setContent(`<div style="text-align:center;"><b>Souřadnice</b><br>MapX: <code>${x}</code><br>MapY: <code>${y}</code></div>`)
             .openOn(map);
-        console.log(`Kliknuto: MapX = ${x}, MapY = ${y}`);
     }
     map.on('click', onMapClick);
 }
 
+// Funkce zoomToLocation zůstává stejná
 function zoomToLocation(x, y) {
     if (myMap && x != null && y != null) {
         var target = myMap.unproject([x, y], myMap.getMaxZoom() - 1);
         myMap.flyTo(target, 1);
-
         var key = x + "_" + y;
         var marker = markers[key];
         if (marker) {
